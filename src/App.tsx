6083907d7436a5cdcf47aa4fe4c688081f3da4d0
Anirudha Bele <beleanirudha8@gmail.com>
2026-02-14 19:24:12 +0530

    check: check shadcn ui and tailwind css working

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { Command, CommandInput } from "@/components/ui/command";
 import "./App.css";
 
 function App() {
 	return (
-		<div>
-			<h1>klicheck - An Advanced Weather Forecast Application</h1>
+		<div className="p-2">
+			<h1 className="text-3xl font-extrabold">
+				klicheck - An Advanced Weather Forecast Application
+			</h1>
+			<div className="flex gap-2 mt-2">
+				<Command className="max-w-sm rounded-lg border">
+					<CommandInput placeholder="Type a city to know weather..." />
+				</Command>
+				<Button variant={"destructive"}>Check</Button>
+			</div>
 		</div>
 	);
 }
