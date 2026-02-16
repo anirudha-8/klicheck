06d5f335576bc685a82e3ef7333ecae7068f9532
Anirudha Bele <beleanirudha8@gmail.com>
2026-02-16 21:05:52 +0530

    feat(src/layouts/Layout.tsx): add header in the app layout

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,9 +1,11 @@
+import Header from "@/components/Header";
 import type { PropsWithChildren } from "react";
 
 const layout = ({ children }: PropsWithChildren) => {
   return (
     <div className="bg-gradient-to-br from-background to-muted">
       {/* header */}
+      <Header />
 
       <main className="min-h-screen container mx-auto px-4 py-8">
         {children}
