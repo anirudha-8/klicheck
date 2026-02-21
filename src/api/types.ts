bcff32f23877e8af2b0e46e1e41ef6133ddd2445
Anirudha Bele <beleanirudha8@gmail.com>
2026-02-21 10:58:59 +0530

    chore(src/api/types.ts): add types for all weather data using interface

--- src/api/types.ts
+++ src/api/types.ts
@@ -9,3 +9,27 @@ export interface WeatherCondition {
   description: string;
   icon: string;
 }
+
+export interface WeatherData {
+  coord: Coordinates;
+  weather: WeatherCondition[];
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+  };
+  sys: {
+    sunrise: number;
+    sunset: number;
+    country: string;
+  };
+  name: string;
+  dt: number;
+}
