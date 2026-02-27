bb7a8320764357d004d729bcc2f65235fa90fb5b
Anirudha Bele <beleanirudha8@gmail.com>
2026-02-27 20:23:30 +0530

    feat(src/pages/WeatherDashboardPage.tsx): add required UI components and icons

--- src/pages/WeatherDashboardPage.tsx
+++ src/pages/WeatherDashboardPage.tsx
@@ -1,3 +1,9 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import WeatherSkeleton from "@/components/WeatherSkeleton";
+import { useGeoLocation } from "@/hooks/useGeoLocation";
+import { AlertTriangle, MapPin, RefreshCw } from "lucide-react";
+
 const WeatherDashboardPage = () => {
   return <div></div>;
 };
