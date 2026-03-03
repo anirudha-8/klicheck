0f73f2f1ad4a609777f0c227455ea1ff3daafe77
Anirudha Bele <beleanirudha8@gmail.com>
2026-03-03 22:57:49 +0530

    feat(src/hooks/useWeatherQuery): add different weather keys for queries

---
+++ src/hooks/useWeatherQuery.ts
@@ -0,0 +1,7 @@
+import type { Coordinates } from "@/api/types";
+
+export const WEATHER_KEYS = {
+  weather: (coords: Coordinates) => ["weather", coords] as const,
+  forecast: (coords: Coordinates) => ["forecast", coords] as const,
+  location: (coords: Coordinates) => ["location", coords] as const,
+} as const;
