297b041e7f532ada62fcd4b44ddc34236c396fbe
Anirudha Bele <beleanirudha8@gmail.com>
2026-03-04 22:58:56 +0530

    feat(src/hooks/useWeatherQuery): add weather query for get current weather

--- src/hooks/useWeatherQuery.ts
+++ src/hooks/useWeatherQuery.ts
@@ -1,7 +1,19 @@
 import type { Coordinates } from "@/api/types";
+import { weatherAPI } from "@/api/weather";
+import { useQuery } from "@tanstack/react-query";
 
 export const WEATHER_KEYS = {
   weather: (coords: Coordinates) => ["weather", coords] as const,
   forecast: (coords: Coordinates) => ["forecast", coords] as const,
   location: (coords: Coordinates) => ["location", coords] as const,
 } as const;
+
+// Query to get current weather data
+export function useWeatherQuery(coordinates: Coordinates | null) {
+  return useQuery({
+    queryKey: WEATHER_KEYS.weather(coordinates ?? { lat: 0, lon: 0 }),
+    queryFn: () =>
+      coordinates ? weatherAPI.getCurrentWeather(coordinates) : null,
+    enabled: !!coordinates,
+  });
+}
